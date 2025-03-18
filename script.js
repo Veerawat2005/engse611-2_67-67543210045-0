1ab541f896b80fdd118a0f02fc167c33f062b4ce
Veerawat2005 <gggfgygfvh45@gmail.com>
2025-03-18 11:26:06 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
- function changeImage(element) {
-        const mainImage = document.getElementById("mainImage");
-        mainImage.style.opacity = 0.5;
-        setTimeout(() => {
-            mainImage.src = element.src;
-            mainImage.style.opacity = 1;
-        }, 200);
-    }
